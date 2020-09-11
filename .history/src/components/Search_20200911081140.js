24e8fa4e11d01fe287117f08c6b146414a09d280
Akinsiku Femi <akinsiku13@gmail.com>
2020-09-11 12:57:02 +0100

    Add markers to the map and correct syntax error in dummy json data

---
+++ .history/src/components/Search_20200911081140.js
@@ -0,0 +1,201 @@
+import React, {useState, useEffect, useContext} from 'react';
+import './Search.scss';
+import TextField from "@material-ui/core/TextField";
+import usePlacesAutocomplete, {
+  getGeocode,
+  getLatLng,
+} from "use-places-autocomplete";
+import {
+  Combobox,
+  ComboboxInput,
+  ComboboxPopover,
+  ComboboxList,
+  ComboboxOption,
+} from "@reach/combobox";
+import "@reach/combobox/styles.css";
+
+import { GoogleMap, useLoadScript, Marker, InfoWindow } from '@react-google-maps/api';
+import { GoogleMap as GoogleMapp, withScriptjs, withGoogleMap } from 'react-google-maps';
+import { formatRelative } from 'date-fns';
+import { LatitudeContext, LongitudeContext } from "./LocationContext";
+import requestData from "./requestData.json";
+
+
+
+
+// export const Map = () => {
+//   const [userLat, setUserLat] = useState(0);
+//     const [userLng, setUserLng] = useState(0);
+    
+
+//   const [requestArr, setRequestArr] = useState(requestData);
+
+
+
+//   useEffect(() => {
+//     getUserLocation();
+//   }, []);
+
+//     const getUserLocation = () => {
+//       window.navigator.geolocation.getCurrentPosition(
+//         (position) => {
+//           let { latitude, longitude } = position.coords;
+//           console.log(latitude, longitude);
+//           setUserLat(latitude);
+//           setUserLng(longitude);
+//         },
+//         (error) => {
+//           if (error.code === 1) {
+//             // setLat(do something)
+//             // setLng(do something)
+//             alert(
+//               "Kindly allow location, for a more immersive experience with the app."
+//             );
+//             console.log(error);
+//           }
+//         }
+//       );
+//     };
+
+//     const libraries = ["places"];
+//     const mapContainerStyle = {
+//       width: "100vw",
+//       height: "100vh",
+//     };
+
+//     const center = {
+//       lat: userLat,
+//       lng: userLng,
+//     };
+
+//   const { isLoaded, loadError } = useLoadScript({
+//     googleMapsApiKey: `AIzaSyBniFhD5gyPyOrEm212cVIAYVythPk2JcE`,
+//     libraries,
+//   });
+
+//   if (loadError) return "Error loading maps";
+//   if (!isLoaded) return "Loading Maps";
+//   console.log(requestArr);
+//   return (
+//     <GoogleMap mapContainerStyle={mapContainerStyle} zoom={8} center={center}>
+//       {requestArr.map((request) => (
+//         <Marker
+//           key={request.id}
+//           position={{ lat: request.location.lat, lng: request.location.lng }}
+//         />
+//       ))}
+//     </GoogleMap>
+//   );
+// };
+
+
+const Map = () => {
+      
+  
+  
+  const center = {
+        lat: userLat,
+        lng: userLng,
+      };
+
+  return <GoogleMapp></GoogleMapp>;
+}
+
+
+
+
+
+export const MapHolder = () => {
+  return <h1>Map here</h1>;
+};
+
+const WrappedMap = withScriptjs(withGoogleMap(Map));
+
+
+
+export const Search =() => {
+    
+
+
+    const [userLat, setUserLat] = useState(0);
+    const [userLng, setUserLng] = useState(0);
+
+    useEffect(() => {
+      getUserLocation();
+    }, []);
+
+    const getUserLocation = () => {
+      window.navigator.geolocation.getCurrentPosition(
+        (position) => {
+          let { latitude, longitude } = position.coords;
+          console.log(latitude, longitude);
+          setUserLat(latitude);
+          setUserLng(longitude);
+        },
+        (error) => {
+          if (error.code === 1) {
+            // setLat(do something)
+            // setLng(do something)
+            alert(
+              "Kindly allow location, for a more immersive experience with the app."
+            );
+            console.log(error);
+          }
+        }
+      );
+    };
+    
+    
+    // change the location later
+    const { ready, value, suggestions: {status, data}, setValue, clearSuggestions } = usePlacesAutocomplete({
+      requestOptions: {
+            location: { lat: () => userLat, lng: () => userLng },
+            radius: 200 * 1000,
+      },
+    });
+    
+    // console.log(userLat)
+    
+    return (
+      <div>
+        <Combobox
+          onSelect={(address) => {
+            console.log(address);
+          }}
+        >
+          {/* <ComboboxInput
+            // disabled={!ready}
+            placeholder="Enter a location"
+            value={value}
+            onChange={(e) => {
+              setValue(e.target.value);
+            }}
+          /> */}
+
+          <TextField
+            // disabled={!ready}
+
+            autoFocus
+            margin="dense"
+            id="location"
+            label="location"
+            type="location"
+            variant="outlined"
+            // fullWidth
+            onChange={(e) => {
+              setValue(e.target.value);
+            }}
+            value={value}
+          />
+
+
+          <ComboboxPopover>
+            {status === "OK" &&
+              data.map((id, description) => (
+                <ComboboxOption key={id} value={description} />
+              ))}
+          </ComboboxPopover>
+        </Combobox>
+      </div>
+    );
+}
